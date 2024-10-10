8c0a7277ab65f7e7b7a87e70717638f45e5f11df
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-11 00:48:55 +0100

    documentation2

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
 def add(a:int, b:int) -> int:
-    """Addition function"""
+    """
+    Returns the sum of a and b.
+
+    Parameters
+    ----------
+    a : int
+        first integer to add
+    b : int
+        second integer to add
+
+    Returns
+    -------
+    int
+        the sum of a and b
+    """
     return a + b
