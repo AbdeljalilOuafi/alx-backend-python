c4767a2ebeb23f28c5f513fc5e05fec737859a91
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-11 01:49:37 +0100

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""5-sum_list module"""
+
+
+def sum_list(input_list: list[float] = []):
+    """
+    Returns the sum of a list of floats.
+
+    Parameters
+    ----------
+    input_list : list[float]
+        list of floats
+
+    Returns
+    -------
+    float
+        the sum of the list
+    """
+    return sum(input_list)
