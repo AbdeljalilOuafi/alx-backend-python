9f3362438a65677651c63b2bfa57631278240151
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-11 02:37:02 +0100

    style

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,3 @@ from typing import Iterable, Sequence, Tuple, List
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Func doc"""
     return [(i, len(i)) for i in lst]
-
