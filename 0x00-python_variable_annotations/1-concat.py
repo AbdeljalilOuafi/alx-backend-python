3c9fa3ec6f9f5c1867c9611675fbdfbec03222b1
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-11 01:03:53 +0100

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns the concatenation of str1 and str2.
+
+    Parameters
+    ----------
+    str1 : str
+        first string
+    str2 : str
+        second string
+
+    Returns
+    -------
+    str
+        the concatenation of str1 and str2
+    """
+    return str1 + str2
