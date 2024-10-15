604f5c02813a23837f7866403f458cdc614f575c
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-15 22:04:46 +0100

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""1-concurrent_coroutines module"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int):
+    """
+    Spawn wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay of wait_random.
+
+    Returns:
+        List[float]: List of all the delays (float values).
+    """
+    delays = []
+    for i in range(n):
+        delays.append(await wait_random(max_delay))
+    return delays
